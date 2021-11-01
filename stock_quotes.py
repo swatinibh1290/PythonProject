ec0e19287fd5434cf0e6eb6cd1c4eb33c76b7b7c
EliseAlonzi <93531654+EliseAlonzi@users.noreply.github.com>
2021-11-01 13:57:42 +0000

    Add files via upload

---
+++ stock_quotes.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 24 15:57:10 2021
+
+Code based on Darren Redmond's example given on in the MIS41110 Lecture 6b, 
+recorded on October 30, 2020. 
+
+@author: Elise Alonzi
+"""
+def display_menu():
+    print('Welcome to the Stock Quotes Analyser')
+    print('Option 1: Gather Data')
+    print('Option 4: Quit')
+    
+def main():
+    #display the menu
+    display_menu()
+    #ask the user for their choice
+    #display the user's choice
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+#see companylist.csv and seaborn tutorial in week 7 folder 
+# use OLS regresssion model to come up with r2 
+
+#graphical UI is expected (lecture 8b in Brightspace)
+
+#tutorials 7 and 8 to use Yahoo finance
+
+#see stockquotes1.py
+#pandas.describe
+#check RMSE
+
+###Github
+#repository
+#clone repository
+#commit changes and pull changes
+#version control through github
+#final product- clone from git
+#pull, push, add, commit
