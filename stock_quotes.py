ba3e5ab0f667f0801acc5e58610cb9870a8a288a
EliseAlonzi <93531654+EliseAlonzi@users.noreply.github.com>
2021-11-08 13:29:04 +0000

    Add files via upload
    testing upload
    take out #comment

--- stock_quotes.py
+++ stock_quotes.py
@@ -3,8 +3,8 @@
 """
 Created on Sun Oct 24 15:57:10 2021
 
-Code based on Darren Redmond's example given on in the MIS41110 Lecture 6b, 
-recorded on October 30, 2020. 
+Code based on Darren Redmond's example given on in the MIS41110 Lecture 6b,
+recorded on October 30, 2020.
 
 @author: Elise Alonzi
 """
@@ -12,7 +12,7 @@ def display_menu():
     print('Welcome to the Stock Quotes Analyser')
     print('Option 1: Gather Data')
     print('Option 4: Quit')
-    
+
 def main():
     #display the menu
     display_menu()
@@ -21,12 +21,12 @@ def main():
 
 if __name__ == "__main__":
     main()
+#comment
 
 
 
-
-#see companylist.csv and seaborn tutorial in week 7 folder 
-# use OLS regresssion model to come up with r2 
+#see companylist.csv and seaborn tutorial in week 7 folder
+# use OLS regresssion model to come up with r2
 
 #graphical UI is expected (lecture 8b in Brightspace)
 
